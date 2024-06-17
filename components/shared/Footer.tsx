cc8cca9951996aee8ff0100b5be1320b3455bdfd
shubham <khobragade.shubham@gmail.com>
2024-06-17 18:27:45 +0530

    make the slight change

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -7,14 +7,15 @@ const Footer = () => {
             <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
                 <Link href='/'>
                     <Image
-                        src="/assets/images/logo.svg"
+                        className="rounded-lg"
+                        src="/assets/images/enroll.png"
                         alt="logo"
                         width={128}
                         height={38}
                     />
                 </Link>
 
-                <p>2023 Evently. All Rights reserved.</p>
+                <p>2023 Enrollify. All Rights reserved.</p>
             </div>
         </footer>
     )
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -11,8 +11,8 @@ const Header = () => {
             <div className="wrapper flex items-center justify-between">
                 <Link href="/" className="w-36">
                     <Image
-                        src="/assets/images/logo.svg" width={128} height={38}
-                        alt="Evently logo"
+                        src="/assets/images/enroll.png" width={128} height={38} className="rounded-lg"
+                        alt="Enrollify logo"
                     />
                 </Link>
 
